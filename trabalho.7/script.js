a9aeff5852bce4991456f26918be8a0ce42c98ed
juansilvarb16 <juansilvarb16@gmail.com>
2026-05-09 18:29:19 -0300

    Update script.js

--- trabalho.7/script.js
+++ trabalho.7/script.js
@@ -1,3 +1,4 @@
+  //questao 3
   //repositorio para checkpoint #1 de engenharia de software
 
  //questao 1
@@ -10,3 +11,71 @@
 //Questão 2
     console.log("Mensagem inicial");
 alert("Bem-vindo ao Checkpoint!");
+
+//questao 4
+var cidade = "São Paulo";
+var anoAtual = 2026;
+ 
+let nomeUsuario = "Juan";
+let saldo = 5000;
+ 
+const cpf = "123.456.789-00";
+const aniversario = "3 de março";
+ 
+console.log(cidade);
+console.log(anoAtual);
+console.log(nomeUsuario);
+console.log(saldo);
+console.log(cpf);
+console.log(aniversario);
+ 
+//questao 5
+let a = 10;
+let b = 2;
+ 
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+ 
+//questao 6
+console.log(7==7); 
+console.log(7===7);
+console.log (8!=8); 
+console.log(9!==9); 
+console.log(8>7);
+ 
+//questao 7
+ 
+let usuario = true;
+let possuiCreditos = false;
+ 
+console.log(usuario && possuiCreditos);
+console.log(usuario || possuiCreditos);
+console.log(!usuario);
+console.log(!possuiCreditos);
+
+//questao 9
+function compararNumeros(a, b) {
+  if (a > b) {
+    return "A é maior";
+  } else if (b > a) {
+    return "B é maior";
+  } else {
+    return "Ambos são iguais";
+  }
+}
+ 
+console.log(compararNumeros(5, 10));
+console.log(compararNumeros(10, 5));
+console.log(compararNumeros(7, 7));
+ 
+//questao 10
+let a = 15;
+let b = 25;
+console.log("Soma:", a + b);
+ 
+let sol = true;
+let chuva = false;
+console.log(sol && chuva);
+console.log(sol || chuva);
