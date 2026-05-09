75b867716934f2a8e47a8901bc2a603eb758bb16
juansilvarb16 <juansilvarb16@gmail.com>
2026-05-09 17:43:12 -0300

    Update script.js

---
+++ trabalho.7/script.js
@@ -0,0 +1,12 @@
+  //repositorio para checkpoint #1 de engenharia de software
+
+ //questao 1
+ //node 24.15.0
+ //npm 9.8.1
+//git version 2.54.0.windows.1
+
+
+
+//Questão 2
+    console.log("Mensagem inicial");
+alert("Bem-vindo ao Checkpoint!");
